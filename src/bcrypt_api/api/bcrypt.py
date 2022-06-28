81b7042486a3c8f6c78f75cbafbb55003439ef5f
ot-nemoto <nemoto@opentone.co.jp>
2022-06-28 03:25:01 +0000

    modify src/bcrypt_api/api/bcrypt.py
    - versionを指定出来るように修正(default: 2a)

--- src/bcrypt_api/api/bcrypt.py
+++ src/bcrypt_api/api/bcrypt.py
@@ -4,11 +4,12 @@ import bcrypt
 
 def generate_hash(**kwargs):
     password = kwargs.get('body').get('password')
-    cost = kwargs.get('body').get('cost')
-    #salt = bcrypt.gensalt(rounds=cost, prefix=b'2a')
+    cost = kwargs.get('body').get('cost', 10)
+    version = kwargs.get('body').get('version', '2a')
+    salt = bcrypt.gensalt(rounds=cost, prefix=version.encode())
     return {
         'password': password,
-        'hash': bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds=cost)).decode()
+        'hash': bcrypt.hashpw(password.encode(), salt).decode(),
     }
 
 def check_password(**kwargs):
